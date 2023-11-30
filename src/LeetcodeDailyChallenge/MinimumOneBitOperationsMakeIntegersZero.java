b422b6db84c8bb49a4dbda3b2bc45a3bb4759847
Sbrao666 <sbrao666@gmail.com>
2023-11-30 21:55:44 +0530

    day 14 : leetcode daily challenge

---
+++ src/LeetcodeDailyChallenge/MinimumOneBitOperationsMakeIntegersZero.java
@@ -0,0 +1,20 @@
+package LeetcodeDailyChallenge;
+
+import java.util.Scanner;
+
+public interface MinimumOneBitOperationsMakeIntegersZero {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(minimumOneBitOperations(n));
+    }
+
+    static int minimumOneBitOperations(int n) {
+        int ans = n;
+        ans ^= ans >> 16;
+        ans ^= ans >> 8;
+        ans ^= ans >> 4;
+        ans ^= ans >> 2;
+        ans ^= ans >> 1;
+        return ans;
+    }
+}
